47f503cc311b7c442cedaa9a9a98e430dba8bb64
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-11 23:23:12 +0530

    useState-obj

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import HookCounter from "./components/HookCounter";
 import HookCounterTwo from "./components/HookCounter2";
+import HookCounter3 from "./components/HookCounter3";
 
 function App() {
   return (
     <div className="App">
       {/* <HookCounter /> */}
-      <HookCounterTwo />
+      {/* <HookCounterTwo /> */}
+      <HookCounter3 />
     </div>
   );
 }
